2ba62bb5b68650e78476a207151137878e630c21
Dariusz Stefanski <d.a.stefanski@gmail.com>
2015-06-10 01:51:49 +0200

    ProxySettingsResolverSpec small refactoring

--- src/test/groovy/com/ofg/uptodate/finder/http/ProxySettingsResolverSpec.groovy
+++ src/test/groovy/com/ofg/uptodate/finder/http/ProxySettingsResolverSpec.groovy
@@ -1,9 +1,10 @@
 package com.ofg.uptodate.finder.http
+
 import com.ofg.uptodate.UptodatePluginExtension
 import spock.lang.Specification
 
 class ProxySettingsResolverSpec extends Specification {
-    
+
     private static final String SYS_PROP_HTTP_PROXY_HOSTNAME = 'http-proxy-sys-prop'
     private static final int SYS_PROP_HTTP_PROXY_PORT = 15000
     private static final String SYS_PROP_HTTPS_PROXY_HOSTNAME = 'https-proxy-sys-prop'
@@ -11,7 +12,7 @@ class ProxySettingsResolverSpec extends Specification {
     private static final String PLUGIN_CONFIG_PROXY_HOSTNAME = 'http-proxy-plugin-config'
     private static final int PLUGIN_CONFIG_PROXY_PORT = 15200
     private static final String PLUGIN_CONFIG_PROXY_SCHEME = 'http'
-    
+
     def "'http.*' system properties should have the highest priority when resolving proxy configuration"() {
         given: "proxy is configured via 'http,*' system properties"
             String prevHttpProxyHost = System.setProperty('http.proxyHost', SYS_PROP_HTTP_PROXY_HOSTNAME)
@@ -20,9 +21,9 @@ class ProxySettingsResolverSpec extends Specification {
             String prevHttpsProxyHost = System.setProperty('https.proxyHost', SYS_PROP_HTTPS_PROXY_HOSTNAME)
             String prevHttpsProxyPort = System.setProperty('https.proxyPort', SYS_PROP_HTTPS_PROXY_PORT.toString())
         and: "proxy is configured via plugin configuration"
-            UptodatePluginExtension uptodatePluginExtension = new UptodatePluginExtension(proxyHostname: PLUGIN_CONFIG_PROXY_HOSTNAME, proxyPort: PLUGIN_CONFIG_PROXY_PORT, proxyScheme: PLUGIN_CONFIG_PROXY_SCHEME)
+            UptodatePluginExtension uptodatePluginExtension = uptodatePluginExtensionWithProxyConfiguredViaPluginExtension()
         expect:
-            new ProxySettings(hostname: SYS_PROP_HTTP_PROXY_HOSTNAME, port: SYS_PROP_HTTP_PROXY_PORT, scheme: 'http') == new ProxySettingsResolver(uptodatePluginExtension).resolve()
+            proxySettingsFor(uptodatePluginExtension) == propsHttpProxySettings()
         cleanup:
             System.setProperty('http.proxyHost', prevHttpProxyHost ?: '')
             System.setProperty('http.proxyPort', prevHttpProxyPort ?: '')
@@ -35,9 +36,9 @@ class ProxySettingsResolverSpec extends Specification {
             String prevHttpsProxyHost = System.setProperty('https.proxyHost', SYS_PROP_HTTPS_PROXY_HOSTNAME)
             String prevHttpsProxyPort = System.setProperty('https.proxyPort', SYS_PROP_HTTPS_PROXY_PORT.toString())
         and: "proxy is configured via plugin configuration"
-            UptodatePluginExtension uptodatePluginExtension = new UptodatePluginExtension(proxyHostname: PLUGIN_CONFIG_PROXY_HOSTNAME, proxyPort: PLUGIN_CONFIG_PROXY_PORT, proxyScheme: PLUGIN_CONFIG_PROXY_SCHEME)
+            UptodatePluginExtension uptodatePluginExtension = uptodatePluginExtensionWithProxyConfiguredViaPluginExtension()
         expect:
-            new ProxySettings(hostname: SYS_PROP_HTTPS_PROXY_HOSTNAME, port: SYS_PROP_HTTPS_PROXY_PORT, scheme: 'https') == new ProxySettingsResolver(uptodatePluginExtension).resolve()
+            proxySettingsFor(uptodatePluginExtension) == propsHttpsProxySettings()
         cleanup:
             System.setProperty('https.proxyHost', prevHttpsProxyHost ?: '')
             System.setProperty('https.proxyPort', prevHttpsProxyPort ?: '')
@@ -45,13 +46,40 @@ class ProxySettingsResolverSpec extends Specification {
 
     def "plugin configuration should have the highest priority when resolving proxy configuration and proxy system properties are not present"() {
         given: "proxy is configured via plugin configuration"
-            UptodatePluginExtension uptodatePluginExtension = new UptodatePluginExtension(proxyHostname: PLUGIN_CONFIG_PROXY_HOSTNAME, proxyPort: PLUGIN_CONFIG_PROXY_PORT, proxyScheme: PLUGIN_CONFIG_PROXY_SCHEME)
+            UptodatePluginExtension uptodatePluginExtension = uptodatePluginExtensionWithProxyConfiguredViaPluginExtension()
         expect:
-            new ProxySettings(hostname: PLUGIN_CONFIG_PROXY_HOSTNAME, port: PLUGIN_CONFIG_PROXY_PORT, scheme: 'http') == new ProxySettingsResolver(uptodatePluginExtension).resolve()
+            proxySettingsFor(uptodatePluginExtension) == pluginProxySettings()
     }
 
     def "should return null when no proxy settings found"() {
         expect:
-            new ProxySettingsResolver(new UptodatePluginExtension()).resolve() == null
+            proxySettingsFor(new UptodatePluginExtension()) == null
+    }
+
+    private UptodatePluginExtension uptodatePluginExtensionWithProxyConfiguredViaPluginExtension() {
+        return new UptodatePluginExtension(
+                proxyHostname: PLUGIN_CONFIG_PROXY_HOSTNAME,
+                proxyPort: PLUGIN_CONFIG_PROXY_PORT,
+                proxyScheme: PLUGIN_CONFIG_PROXY_SCHEME
+        )
+    }
+
+    private ProxySettings proxySettingsFor(UptodatePluginExtension uptodatePluginExtension) {
+        return new ProxySettingsResolver(uptodatePluginExtension).resolve()
+    }
+
+    private ProxySettings propsHttpProxySettings() {
+        return new ProxySettings(hostname: SYS_PROP_HTTP_PROXY_HOSTNAME, port: SYS_PROP_HTTP_PROXY_PORT, scheme: 'http')
+    }
+
+    private ProxySettings propsHttpsProxySettings() {
+        return new ProxySettings(hostname: SYS_PROP_HTTPS_PROXY_HOSTNAME, port: SYS_PROP_HTTPS_PROXY_PORT, scheme: 'https')
+    }
+
+    private ProxySettings pluginProxySettings() {
+        return new ProxySettings(
+                hostname: PLUGIN_CONFIG_PROXY_HOSTNAME,
+                port: PLUGIN_CONFIG_PROXY_PORT,
+                scheme: PLUGIN_CONFIG_PROXY_SCHEME)
     }
 }
