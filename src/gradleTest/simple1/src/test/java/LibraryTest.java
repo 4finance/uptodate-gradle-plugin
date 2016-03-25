208a6edf135e7a19edd0919ead088d28de8a7d53
Marcin Zajaczkowski <marcin.zajaczkowski@4finance.com>
2016-03-25 13:24:29 +0100

    Reproduce issue with gradle-test
    Tested plugin itself is not added as a dependency.

---
+++ src/gradleTest/simple1/src/test/java/LibraryTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class LibraryTest {
+    @Test public void testSomeLibraryMethod() {
+        Library classUnderTest = new Library();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+}
